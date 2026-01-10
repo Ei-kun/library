2fe55e09b402bfe206e08e6bf2a784aedd3c32d8
Ei-kun <231045566+Ei-kun@users.noreply.github.com>
2026-01-10 23:21:16 +0530

    Add book form modal UI

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ function Book(author,title,page,read){
 function addBookToLibrary(){
     
 }
+
+const showInputDialog=document.querySelector(".add-button");
+const closeInputDialog=document.querySelectorAll(".dialog-close")
+const dialog=document.querySelector("#add-form");
+const form=document.querySelectorAll("form");
+
+showInputDialog.addEventListener("click",() =>{
+    dialog.showModal();
+});
+
+closeInputDialog.forEach(button =>{
+    button.addEventListener("click",()=>{
+        dialog.close();
+    })
+});
+
+dialog.addEventListener("close", () => {
+    form.forEach(item => {
+        item.reset();
+    })
+});
